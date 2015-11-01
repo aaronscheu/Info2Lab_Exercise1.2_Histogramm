11ca7f253b9a3a42e215e8ba84e78e5860dbab47
amaridev <amaridev@ceyu.de>
2015-11-01 17:43:40 +0100

    Initial Commit

--- src/AnalyzeTextFile.java
+++ src/AnalyzeTextFile.java
@@ -18,16 +18,19 @@ public class AnalyzeTextFile {
 
     public AnalyzeTextFile(String pathIn, String pathOut) throws IOException{
         writeFile(pathOut, analyze(readFile(pathIn)));
+        //writeFile(pathOut, readFile(pathIn));
+        //System.out.println(readFile(pathIn));
     }
 
     public String analyze(String inputString){
-        String sOutputString = "";
-        int[] iaTable = new int[26];
+        String sOutputString = "Zeichen insgesamt: " + inputString.length() + "\n";
         HashMap <Integer, Integer> hashMap = new HashMap<>();
 
         for (int i = 0; i < inputString.length(); i++) {
             char c = inputString.charAt(i);
-            if (c != ' ') {
+            if (c >= 65 && c <= 90)
+                c += 32;
+            if (c >= 97 && c <= 122) {
                 // Increment existing value in HashMap.
                 // ... Start with zero if no key exists.
                 int value = hashMap.getOrDefault((int) c, 0);
@@ -42,6 +45,7 @@ public class AnalyzeTextFile {
     }
 
     private String readFile(String path) throws IOException{
+
         BufferedReader br = new BufferedReader(new FileReader(path));
         String sOut = "", line;
         while (br.ready()) {
@@ -51,11 +55,30 @@ public class AnalyzeTextFile {
         }
         br.close();
         return sOut;
+
+        /*
+        //Version with line-break
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        try {
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+
+            while (line != null) {
+                sb.append(line);
+                sb.append("\n");
+                line = br.readLine();
+            }
+            return sb.toString();
+        } finally {
+            br.close();
+        }
+        */
     }
 
     private void writeFile(String path, String sWrite) throws IOException{
         PrintWriter writer = new PrintWriter(path);
         writer.write(sWrite);
+        writer.close();
     }
 }
 
